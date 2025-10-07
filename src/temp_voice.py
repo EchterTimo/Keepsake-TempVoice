098d6f2b615f74fe7595acbf8de82e150b057419
EchterTimo <EchterTimo@users.noreply.github.com>
2025-10-07 19:29:08 +0200

    fix: update permissions logging to include link for better clarity

--- src/temp_voice.py
+++ src/temp_voice.py
@@ -50,7 +50,8 @@ class TempVoice(Extension):
             print(
                 f"[ERROR] Guild with ID {GUILD_ID} not found! The bot needs to be installed in that guild.")
             return
-        print("PERMISSIONS:", guild.permissions)
+        print(
+            f"PERMISSIONS: https://discordapi.com/permissions.html#{guild.permissions}")
         await self.force_fetch_category_data(guild)
 
     @listen(Startup)
